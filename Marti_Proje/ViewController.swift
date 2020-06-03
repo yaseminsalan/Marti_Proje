6ed3747141f2545533b54878a5e06a61c58b75b6
yaseminsalan <yaseminsalann@gmail.com>
2020-06-03 19:29:28 +0300

    Initial Commit

---
+++ Marti_Proje/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Marti_Proje
+//
+//  Created by Yasemin salan on 3.06.2020.
+//  Copyright © 2020 Yasemin salan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Marti_ProjeTests/Marti_ProjeTests.swift
@@ -0,0 +1,34 @@
+//
+//  Marti_ProjeTests.swift
+//  Marti_ProjeTests
+//
+//  Created by Yasemin salan on 3.06.2020.
+//  Copyright © 2020 Yasemin salan. All rights reserved.
+//
+
+import XCTest
+@testable import Marti_Proje
+
+class Marti_ProjeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
